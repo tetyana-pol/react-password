305a9e0eb2dbd0b92877e39733eeaa1895e71993
Tetiana Poletaieva <ptupob@gmail.com>
2024-03-23 20:08:02 +0200

    change modal style

--- src/services/errorService.js
+++ src/services/errorService.js
@@ -1,4 +1,4 @@
 export const errorService = (value) => {
   if (typeof value === "string") return value;
-  return `fielnd_name: ${value[0].field_name} , error ${value[0].error}`;
+  return `fielnd_name: ${value[0].field_name} , error: ${value[0].error}`;
 };
